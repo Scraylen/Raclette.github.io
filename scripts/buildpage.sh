f937996fdff73e9271f37e3fcd49a7b7fd11f819
Céline <celine.douangpaseut@etud.univ-paris8.fr>
2026-02-27 17:41:36 +0100

    Trying to figure out how to export old gitlab website to github

---
+++ scripts/buildpage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function buildpage {
+    FILE="${1#pages/}"
+    TITLE=$(head -n 1 "${1}")
+    DATE=$(date +"%d/%m/%Y")
+
+    cat layout/before.html \
+        | sed "s~{{TITLE}}~${TITLE}~" \
+        | sed "s~href=\"${FILE}\"~& class=\"current\"~"
+    tail -n +2 "${1}"
+    cat layout/after.html \
+        | sed "s~{{DATE}}~${DATE}~"
+}
+
+if [[ "${1}" = "" ]]; then # premier argument obligatoire : le nom de la page
+    echo "Usage: ${0} PAGE" >&2
+    exit 1
+elif test ! -f "${1}"; then # et doit correspondre à une page existante
+    echo "${0}: error: ${1}: no such file" >&2
+    exit 1
+else # si tout va bien on construit la page dans public/
+    FILE="${1#pages/}"
+    buildpage "${1}" > "public/${FILE}"
+fi
